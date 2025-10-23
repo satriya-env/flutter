e8a20bdb83c69a2847753af4321f1c8bbbddc330
Kkiiiiiii <rikizhkria@gmail.com>
2025-10-23 18:23:32 +0700

    .

--- lib/objek.dart
+++ lib/objek.dart
@@ -7,35 +7,73 @@ class Objek extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Flutter Layout',
-            style: 
-            TextStyle(fontFamily: 'coco'),
-            ),
-        ),
+        title: Text('Flutter Layout', style: TextStyle(fontFamily: 'coco'),),
+        centerTitle: true,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.asset('aset/img/objekw.jpg'),
-          SizedBox(height: 10,),
-          Row(
+          Image.asset(  
+            'aset/img/objekw.jpg',
+            width: double.infinity,
+            height: 250,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            margin: EdgeInsets.all(30),
+            child: Row(
               children: [
-                Padding(padding: EdgeInsetsGeometry.all(25)),
                 Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                  Text('Floating House Lembang', style:  TextStyle(fontFamily: 'coco', fontWeight: FontWeight.w500),),
-                  Text('Lembang, Bandung', style: TextStyle(color: Colors.grey, fontFamily: 'coco', fontSize: 12),),
+                    Text('Floating House Lembang', style: TextStyle(fontFamily: 'coco', fontWeight: FontWeight.w500),),
+                    Text('Lembang, Bandung', style: TextStyle(fontFamily: 'coco', fontSize: 12, color: const Color.fromARGB(255, 119, 119, 119)),),
                   ],
                 ),
-                SizedBox(width: 100,),
-                Icon(Icons.star, color: Colors.orange,),
+                SizedBox(width: 125,),
+                Icon( Icons.star, color: Colors.deepOrange,),
                 Text('77')
+              ],
+            ),
+          ),
+          SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Icon(Icons.call, color: Colors.blue,),
+                  SizedBox(height: 8,),
+                  Text('CALL', style: TextStyle(fontFamily: 'coco',),)
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.near_me, color: Colors.blue,),
+                  SizedBox(height: 8,),
+                  Text('ROUTE', style: TextStyle(fontFamily: 'coco',),)
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.share, color: Colors.blue,),
+                  SizedBox(height: 8,),
+                  Text('SHARE', style: TextStyle(fontFamily: 'coco',),)
+                ],
+              ),
             ],
-          ), 
+          ),
+          SizedBox(height: 10,),
+          Container(
+            margin: EdgeInsets.all(32),
+            child: Text(
+              'Floating House Lembang merupakan salah satu destinasi wisata yang menawarkan pengalaman menginap di rumah terapung di atas danau buatan. Terletak di kawasan Lembang, Bandung, tempat ini menawarkan pemandangan alam yang indah serta suasana yang tenang dan damai. Pengunjung dapat menikmati berbagai aktivitas seperti memancing, berperahu, atau sekadar bersantai menikmati keindahan alam sekitar.',
+              style: TextStyle(fontFamily: 'coco', fontSize: 14,),
+              textAlign: TextAlign.justify,
+            ),
+          )
         ],
-      )
+      ),
     );
   }
 }
